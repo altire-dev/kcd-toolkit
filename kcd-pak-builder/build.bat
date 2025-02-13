2aa15e8db4283174e4ac504067b47d47886e63c5
Altire <altire.dev@gmail.com>
2025-02-13 08:54:55 +0000

    BVN

--- kcd-pak-builder/build.bat
+++ kcd-pak-builder/build.bat
@@ -1,7 +1,7 @@
 pyinstaller ^
     --noupx^
     -F --noconsole --clean ^
-    -n kcd-pak-builder-v1.0.0^
+    -n kcd-pak-builder-v1.0.1^
     --upx-dir=..\resources\upx-4.2.4-win64^
     --icon=.\kcd_pak_builder\resources\icon.ico^
     --add-data kcd_pak_builder\resources\icon.ico:resources^
