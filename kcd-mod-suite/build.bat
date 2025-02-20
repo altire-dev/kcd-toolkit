bf92384c0383911de458a1c7c4865c7cad44f990
Altire <altire.dev@gmail.com>
2025-02-20 18:21:17 +0000

    Fixed Mod Generator lib issue

--- kcd-mod-suite/build.bat
+++ kcd-mod-suite/build.bat
@@ -5,5 +5,7 @@ pyinstaller ^
     --upx-dir=..\resources\upx-4.2.4-win64^
     --paths ..\kcd-mod-generator^
     --paths ..\kcd-pak-builder^
+    --paths ..\kcd-asset-finder^
+    --paths ..\kcd-utils^
     --icon=.\kcd_mod_suite\resources\icon.ico^
     .\main.py
