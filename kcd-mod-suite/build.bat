c49d3fcc7cd0d9c041c3e8ab4404a764ae9ae4c3
Altire <altire.dev@gmail.com>
2025-02-20 17:57:30 +0000

    Asset Finder build script

---
+++ kcd-mod-suite/build.bat
@@ -0,0 +1,9 @@
+pyinstaller ^
+    --noupx^
+    -F --noconsole --clean ^
+    -n kcd-mod-suite-v1.0.0^
+    --upx-dir=..\resources\upx-4.2.4-win64^
+    --paths ..\kcd-mod-generator^
+    --paths ..\kcd-pak-builder^
+    --icon=.\kcd_mod_suite\resources\icon.ico^
+    .\main.py
