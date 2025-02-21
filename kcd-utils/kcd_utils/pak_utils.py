9fdc1d5afd06913389975dafcbb505baed29be39
Altire <altire.dev@gmail.com>
2025-02-21 18:40:28 +0000

    Created KCD Utils library

--- kcd-utils/kcd_utils/pak_utils.py
+++ kcd-utils/kcd_utils/pak_utils.py
@@ -6,6 +6,7 @@
 # ===================================================================================================
 import zipfile
 import os
+import shutil
 from zipfile import ZipFile
 
 # ===================================================================================================
@@ -35,14 +36,37 @@ def find_paks(target_dir):
 
     return paks
 
-def get_pak_file_list(target_pak):
+def get_pak_assets(target_pak):
     '''
-    Get a list of files in the PAK at the specified location
+    Get a list of assets/files in the PAK at the specified location
 
     :param target_pak: The absolute path to the target PAK
     :type target_pak: str
-    :return: A full list of files in the target PAK
+    :return: A full list of assets in the target PAK
     :rtype list:
     '''
     pak = ZipFile(target_pak)
     return pak.namelist()
+
+def export_pak_asset(export_path, pak_path, asset_path):
+    '''
+    Exports an asset from a PAK file
+
+    :param pak_path: The path to the target PAK file
+    :type pak_path: str
+    :param asset_path: The path to the asset to export
+    :type asset_path: str
+    '''
+    print("Export %s from pak %s" % (asset_path, pak_path))
+    asset_filename = asset_path.split("/")[-1]
+    out_file = os.path.join(export_path, asset_filename)
+    print("Outputing to %s" % out_file)
+
+    # ===================================================================================================
+    # Read Asset from PAK
+    # ===================================================================================================
+    with zipfile.ZipFile(pak_path) as pak_file:
+        with pak_file.open(asset_path) as asset_file, open(out_file, 'wb') as out_file:
+            shutil.copyfileobj(asset_file, out_file)
+
+
---
+++ kcd-utils/kcd_utils/utils.py
@@ -0,0 +1,35 @@
+# ===================================================================================================
+# General Utilities and Helpers
+# ===================================================================================================
+
+# ===================================================================================================
+# Imports: External
+# ===================================================================================================
+import os
+import psutil
+
+# ===================================================================================================
+# Utility/Helper Methods
+# ===================================================================================================
+def find_kcd2_path():
+    '''
+    Searches the local system for the KCD2 installation path
+
+    :return: The KCD2 installation path, if found, otherwise None
+    :rtype: str
+    '''
+    path = None
+    kcd2_rel_path = os.path.join("steamapps", "common", "KingdomComeDeliverance2")
+
+    for disk in psutil.disk_partitions():
+        kcd2_abs_path = None
+        if disk.mountpoint == "C:\\":
+            kcd2_abs_path = os.path.join(disk.mountpoint, "Program Files (x86)", "Steam", kcd2_rel_path)
+        else:
+            kcd2_abs_path = os.path.join(disk.mountpoint, "SteamLibrary", kcd2_rel_path)
+
+        if os.path.isdir(kcd2_abs_path):
+            path = kcd2_abs_path
+            break
+
+    return path
